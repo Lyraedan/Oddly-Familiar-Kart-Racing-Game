aefe885ac9a6d36cebcbf9a430169baac2f31fbf
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2026-03-07 03:48:16 +0000

    Removed wrong comments

--- 3D Mario Kart/Assets/Scripts/Kart/RacerConfig.cs
+++ 3D Mario Kart/Assets/Scripts/Kart/RacerConfig.cs
@@ -8,8 +8,8 @@ public class RacerConfig : MonoBehaviour
     public Animator Driver;
     public Sprite Emblem;
     public Sprite MinimapIcon;
-    public string CharacterName = "Racer"; // TODO Wire up
-    public Sprite CharacterIcon; // TODO Wire up
+    public string CharacterName = "Racer";
+    public Sprite CharacterIcon;
 
     [System.Serializable]
     public class FaceConfig
